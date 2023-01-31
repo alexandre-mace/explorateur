33a96cd618fa5cba81605395f4afdb82390de085
alexandre-mace <helloalexandremace@protonmail.com>
2023-01-31 10:46:12 +0100

    Update to react 18

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
